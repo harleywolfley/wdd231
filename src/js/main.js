115e6c091c5733b442924d8e62334da505a70e5a
harleywolfley <harleywolfley@gmail.com>
2026-05-09 14:07:29 -0600

    2.2

--- src/js/main.js
+++ src/js/main.js
@@ -7,7 +7,33 @@ function updateParkInformation() {
 
 updateParkInformation()
 
-addEventListener
+function addEventListeners() {
+    const menuTrigger = document.querySelector("#header-menu-trigger");
+    const menuOptions = document.querySelector("#header-menu-options");
+    const overview = document.querySelector(".overview");
+
+    if (menuTrigger && menuOptions) {
+        menuTrigger.addEventListener("click", () => {
+            menuOptions.classList.toggle("is-hidden");
+        });
+        menuOptions.addEventListener("click", (event) => {
+            const itemName = event.target.textContent;
+            console.log(itemName);
+        })
+    }
+
+    if (overview) {
+        overview.addEventListener("mouseenter", ()=> {
+            overview.classList.add("overlay-hover");
+        });
+
+        overview.addEventListener("mouseleave", () => {
+            overview.classList.remove("overlay-hover");
+        });
+    }
+}
+
+addEventListeners()
 
 // Notes
 // document.querySelector("#parkStates").innerHTML = "<p>ID, MT, WY</p>" ---- innerHTML allows us to add tags too
